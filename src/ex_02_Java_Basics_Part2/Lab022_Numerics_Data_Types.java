dd892546547bbfdd6119fd25710606513e985f8b
Manisha Mohanty <manisha.mohanty824@gmail.com>
2025-05-14 12:07:43 +0530

    ex_02_Java_Basics_Part2

---
+++ src/ex_02_Java_Basics_Part2/Lab022_Numerics_Data_Types.java
@@ -0,0 +1,21 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab022_Numerics_Data_Types {
+    public static void main(String[] args) {
+
+        //byte b = 128; // -128 to 127
+        byte b = 10;
+        int i = 10;
+        short s = 10;
+        char c = 'A';
+        long l = 9742498163l;
+        long l1 = 9742498163l;
+
+        float f = 3.14f;
+        float f1 = 3.14F;
+
+        double d = 3.12478299973;
+
+
+    }
+}
