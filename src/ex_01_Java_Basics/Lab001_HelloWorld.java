d91299b439fcbd5dda96cf091605cb7b4bdc8928
Manisha Mohanty <manisha.mohanty824@gmail.com>
2025-05-14 19:02:01 +0530

    10th May 2025

---
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi, Manisha");
+
+        // This is Pramod, This code will not executed
+
+        /*
+         * Author :  Manisha Mohanty
+         *
+         *
+         * */
+
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab008_Keywords_Identifiers.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class Lab008_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Hi, Keyword!");
+    }
+}
