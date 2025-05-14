4a56af416975bc5e0dbf298db0708f963b2ee39a
Manisha Mohanty <manisha.mohanty824@gmail.com>
2025-05-14 16:11:33 +0530

    11th may 5 table using printf

---
+++ src/ex_02_Java_Basics_Part2/Table_of_5.java
@@ -0,0 +1,20 @@
+package ex_02_Java_Basics_Part2;
+
+public class Table_of_5 {
+    public static void main(String[] args) {
+        int num = 5;
+        char new_line='\n';
+
+        System.out.printf("%d*1",num);
+        System.out.printf(new_line+"%d*2",num);
+        System.out.printf(new_line+"%d*3",num);
+        System.out.printf(new_line+"%d*4",num);
+        System.out.printf(new_line+"%d*5",num);
+        System.out.printf(new_line+"%d*6",num);
+        System.out.printf(new_line+"%d*7",num);
+        System.out.printf(new_line+"%d*8",num);
+        System.out.printf(new_line+"%d*9",num);
+        System.out.printf(new_line+"%d*10",num);
+    }
+    }
+
