c725df281237c34f89259a186ddbb9e4d3cd2c5b
Manisha Mohanty <manisha.mohanty824@gmail.com>
2025-06-21 12:06:05 +0530

    21 june 2025- coding challenge10

---
+++ src/Tasks/challenge10coding.java
@@ -0,0 +1,52 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class challenge10coding {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Input operator
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator! Please use +, -, *, or /.");
+        }
+
+        scanner.close();
+    }
+}
+
+
+
