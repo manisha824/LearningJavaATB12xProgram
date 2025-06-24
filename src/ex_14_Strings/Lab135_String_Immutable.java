a81edb71aeee0d08e19f7b246db752e900f10bc0
Manisha Mohanty <manisha.mohanty824@gmail.com>
2025-06-24 23:58:48 +0530

    25th May String

---
+++ src/ex_14_Strings/Lab135_String_Immutable.java
@@ -0,0 +1,10 @@
+package ex_14_Strings;
+
+public class Lab135_String_Immutable {
+    public static void main(String[] args) {
+        String name = "Manisha"; // SCP
+        name.toUpperCase();
+        System.out.println(name); //Manisha x
+
+    }
+}
